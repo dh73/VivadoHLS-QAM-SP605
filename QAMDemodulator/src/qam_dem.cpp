33ac0a73b6b52fe49b875c1c55846769486b1fde
Diego-HR <dhdezr@gmail.com>
2015-01-23 00:04:34 -0600

    Best Coding style and separate the functions inside qam_dem.h to .cpp

--- QAMDemodulator/src/qam_dem.cpp
+++ QAMDemodulator/src/qam_dem.cpp
@@ -1,15 +1,10 @@
 #include "qam_dem.h"
 #include "synt_types.h"
 
-void qam_dem_top( cdata_t     din, 
-    cdata_t    *dout_mix,
-    cphase_t    ph_in,
-    cphase_t   *ph_out,
-    loop_int_t *loop_integ, 
-    ctl_crec_t  control ){
+void qam_dem_top( cdata_t din, cdata_t *dout_mix, cphase_t ph_in, cphase_t *ph_out, loop_int_t *loop_integ,  ctl_crec_t  control ){
 
-	static QAMDEM qam;
+	static QAMDEM qam; //Accesible for all the functions
 
-	qam.mounstrito(din, dout_mix, ph_in, ph_out, loop_integ, control);
+	qam.mounstrito(din, dout_mix, ph_in, ph_out, loop_integ, control); //Magic box :-)
 
 }
